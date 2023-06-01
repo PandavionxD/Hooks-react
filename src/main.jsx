947bb9e033078b3c675389d8bc7138983749b2f4
Alex Daniel <1151372@senati.pe>
2023-06-01 16:18:32 -0500

    Empezando con UseContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { BrowserRouter } from "react-router-dom"
 // import { UseRef } from './04.useRef/UseRef'
-import { TodoApp } from './05.useReduce/TodoApp'
+// import { TodoApp } from './05.useReduce/TodoApp'
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01.useState/CounterApp'
 // import CounterWithApp from './01.useState/CounterWithApp'
 // import { SimpleForm } from './02.useEffect/SimpleForm'
 // import { FormWithCustomHook } from './02.useEffect/FormWithCustomHook'
 // import { CustomHook } from './03.usehookfetch/CustomHook'
+import { MainApp } from './06.useContext/MainApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-  <TodoApp/>
+  <BrowserRouter>
+    <MainApp />
+  </BrowserRouter>
 )
